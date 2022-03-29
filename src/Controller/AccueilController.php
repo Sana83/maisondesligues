635ef137ef094a619fda8411aaf01dab58e6d5a5
Sana83 <83754212+Sana83@users.noreply.github.com>
2022-03-29 14:43:35 +0200

    afficher la page d'accueil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\SearchController;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+}
