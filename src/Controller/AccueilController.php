f93280002656ba9cdf7f7157f053250cfc04e803
Sana83 <83754212+Sana83@users.noreply.github.com>
2022-03-31 10:36:51 +0200

    chemin page

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -18,4 +18,14 @@ class AccueilController extends AbstractController
             'controller_name' => 'AccueilController',
         ]);
     }
+    
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(): Response
+    {
+        return $this->render('accueil/test.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
 }
