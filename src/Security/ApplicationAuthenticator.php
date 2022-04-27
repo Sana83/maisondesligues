b7d756d24352af84f4bdf996dbc4a10bb158eb97
Sana83 <83754212+Sana83@users.noreply.github.com>
2022-04-27 16:35:28 +0100

    page login

--- src/Security/ApplicationAuthenticator.php
+++ src/Security/ApplicationAuthenticator.php
@@ -96,7 +96,8 @@ class ApplicationAuthenticator extends AbstractFormLoginAuthenticator implements
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('maisondesligues'));
     }
 
     protected function getLoginUrl()
