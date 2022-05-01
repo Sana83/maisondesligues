c5f3638aa3cc28b5b1f337dbc00469d066090ffd
Tangui Hoarau <83754212+Sana83@users.noreply.github.com>
2022-05-01 02:01:29 +0200

    page connexion

--- src/Security/ApplicationAuthenticator.php
+++ src/Security/ApplicationAuthenticator.php
@@ -97,7 +97,7 @@ class ApplicationAuthenticator extends AbstractFormLoginAuthenticator implements
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('maisondesligues'));
+        return new RedirectResponse($this->urlGenerator->generate('accueil'));
     }
 
     protected function getLoginUrl()
