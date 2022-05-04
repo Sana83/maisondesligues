92fd08a8664dbb01e094495cffc322a15f369db1
ErwanLambert <erwanlambert@hotmail.fr>
2022-05-04 17:41:08 +0200

    ajout fonctionnalité page accueil plus affichage ateliers

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Controller\SearchController;
 use App\Repository\AtelierRepository;
+use App\Repository\ThemeRepository;
 
 class AccueilController extends AbstractController
 {
@@ -29,5 +30,25 @@ class AccueilController extends AbstractController
             return $this->render('accueil/pageAccueilConnecte.html.twig', [
                 'lesAteliers' => $lesAteliers,
             ]);
+    }
+    
+    /**
+     * @Route("/atelier", name="pageAtelier")
+     */
+    public function atelier(AtelierRepository $repo): Response
+    {
+        $lesAteliers = $repo->findAll();
+            return $this->render('accueil/atelier.html.twig', [
+                'lesAteliers' => $lesAteliers,
+            ]);
+    }
+    
+    /**
+     * @Route("/theme", name="pageTheme")
+     */
+    public function theme(ThemeRepository $repo, AtelierRepository $repoA): Response{
+        $lesThemes = $repo->findBy(
+                ['']
+        );
     }
 }
